92bc519c0ba8dbdce28ad63daf6472cb943a46b1
hilarious3 <shahzad20022002@gmail.com>
2023-01-14 21:26:45 +0530

    java program to add two no.

--- src/practice.java
+++ src/practice.java
@@ -1,20 +1,15 @@
 
 
-//Java Program to Swap Two Numbers
+//Java Program to Add two no.
 
 class practice{
-    static void swaps(int a,int b){
-        int temp=a;
-        a=b;
-        b=temp;
-        System.out.println("The a after swap is : "+a + "the b after swap is : "+ b);
-    }
     public static void main(String[]args){
         int a=3,b=9;
-        swaps(a,b);
+        System.out.println(a+b);
     }
-
 }
+
+
 
 
 
