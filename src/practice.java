894acf32afbae9607b24caf081949714ed9269eb
hilarious3 <shahzad20022002@gmail.com>
2023-01-15 10:06:14 +0530

    print first n natural number in java

--- src/practice.java
+++ src/practice.java
@@ -1,6 +1,6 @@
 
 
-//Odd or Even program in java
+//print first n natural number in java
 
 import java.util.Scanner;
 
@@ -8,14 +8,13 @@ class practice{
     public static void main(String[]args){
         Scanner s=new Scanner(System.in);
         int a=s.nextInt();
-        if(a%2==0){
-            System.out.println("the no. is even");
-        }else{
-            System.out.println("the no.is odd");
+        for(int i=1;i<=a;i++){
+            System.out.println(i + " ");
         }
     }
-
 }
+
+
 
 
 
