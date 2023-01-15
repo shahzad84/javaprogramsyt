3e69ee64fff1348e9437a644a63b54e374b3b214
hilarious3 <shahzad20022002@gmail.com>
2023-01-15 15:46:06 +0530

    Sum of First N natural numbers in java

--- src/practice.java
+++ src/practice.java
@@ -1,18 +1,23 @@
 
 
-//print first n natural number in java
 
+
+
+//Sum of First N natural numbers in java
 import java.util.Scanner;
-
 class practice{
     public static void main(String[]args){
         Scanner s=new Scanner(System.in);
         int a=s.nextInt();
+        int sum=0;
         for(int i=1;i<=a;i++){
-            System.out.println(i + " ");
+            sum=sum+i;
         }
+        System.out.println("the sum is "+sum);
     }
 }
+
+
 
 
 
